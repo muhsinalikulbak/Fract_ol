f6d41e122d2f97353eb80af1cef93f722724ee4e
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-03-16 02:53:24 +0300

    Pencere boyama işlemi

--- fract_ol.c
+++ fract_ol.c
@@ -6,16 +6,38 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 19:20:28 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/03/14 02:47:36 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/03/16 02:52:48 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract_ol.h"
 
-int main()
-{
-	void *init =  mlx_init();
-	mlx_new_window(init, 600, 800, "Muhsin");
-	mlx_loop(init);
 
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int*)dst = color;
+}
+
+int	main(void)
+{
+	void	*mlx;
+	void	*mlx_win;
+	t_data	img;
+
+	mlx = mlx_init();
+	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
+	img.img = mlx_new_image(mlx, 1920, 1080);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+	for (int y = 0; y < 1080; y++) 
+	{
+		for (int x = 0; x < 1920; x++) 
+		{
+			my_mlx_pixel_put(&img, x, y, x+y+(x)*73); // Kırmızı renk
+		}
+	}	
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx_loop(mlx);
 }
--- fract_ol.h
+++ fract_ol.h
@@ -6,13 +6,26 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 19:20:24 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/03/14 00:35:14 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/03/16 02:52:17 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACT_OL_H
 # define FRACT_OL_H
+# define WIDTH 1920
+# define HEIGT 1080
 # include "minilibx-linux/mlx.h"
 # include  "libft/libft.h"
+
+typedef struct	s_data
+{
+	void	*init;
+	void	*img;
+	void	*mlx_win;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}				t_data;
 
 #endif
