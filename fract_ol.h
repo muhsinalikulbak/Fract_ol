9985e86b6ee63084beb0b3acdff45e73dfc1b54b
Muhsin Ali Kulbak <muhsin.kulbak@gmail.com>
2025-03-19 20:26:53 +0300

    küme struct isimlendirmeleri düzeltildi

--- fract_ol.h
+++ fract_ol.h
@@ -6,14 +6,14 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 19:20:24 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/03/19 03:11:21 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/03/19 20:08:13 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACT_OL_H
 # define FRACT_OL_H
-# define WIDTH 500
-# define HEIGHT 500
+# define WIDTH 1920
+# define HEIGHT 1080
 # define MANDELBROT 1
 # define JULIA 2
 # define RESET   "\033[0m"
--- fractals.c
+++ fractals.c
@@ -6,57 +6,59 @@
 /*   By: mkulbak <mkulbak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/17 14:55:41 by mkulbak           #+#    #+#             */
-/*   Updated: 2025/03/19 03:09:30 by mkulbak          ###   ########.fr       */
+/*   Updated: 2025/03/19 20:26:00 by mkulbak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fract_ol.h"
 
-static int	julia_equation(t_set_data *c, int x, int y)
+static int	julia_equation(t_set_data *set, int x, int y)
 {
 	int		iter;
 	double	temp_reel;
 
 	iter = 1;
-	c->z_re = c->x_min + ((double)x / WIDTH) * (c->x_max - c->x_min);
-	c->z_im = c->y_min + ((double)y / HEIGHT) * (c->y_max - c->y_min);
-	c->c_re = c->julia_re;
-	c->c_im = c->julia_im;
-	while (iter <= c->iteration)
+	set->z_re = set->x_min + ((double)x / WIDTH) * (set->x_max - set->x_min);
+	set->z_im = set->y_min + ((double)y / HEIGHT) * (set->y_max - set->y_min);
+	set->c_re = set->julia_re;
+	set->c_im = set->julia_im;
+	while (iter <= set->iteration)
 	{
-		temp_reel = c->z_re;
-		c->z_re = (c->z_re * c->z_re) - (c->z_im * c->z_im) + c->c_re;
-		c->z_im = (2 * temp_reel * c->z_im) + c->c_im;
-		if ((c->z_re * c->z_re + c->z_im * c->z_im) > 4.0)
+		temp_reel = set->z_re;
+		set->z_re = (
+				set->z_re * set->z_re) - (set->z_im * set->z_im) + set->c_re;
+		set->z_im = (2 * temp_reel * set->z_im) + set->c_im;
+		if ((set->z_re * set->z_re + set->z_im * set->z_im) > 4.0)
 			return (iter);
 		iter++;
 	}
 	return (iter);
 }
 
-static int	mandel_equation(t_set_data *c, int x, int y)
+static int	mandel_equation(t_set_data *set, int x, int y)
 {
 	int		iter;
 	double	temp_reel;
 
 	iter = 1;
-	c->z_re = 0;
-	c->z_im = 0;
-	c->c_re = c->x_min + ((double)x / WIDTH) * (c->x_max - c->x_min);
-	c->c_im = c->y_min + ((double)y / HEIGHT) * (c->y_max - c->y_min);
-	while (iter <= c->iteration)
+	set->z_re = 0;
+	set->z_im = 0;
+	set->c_re = set->x_min + ((double)x / WIDTH) * (set->x_max - set->x_min);
+	set->c_im = set->y_min + ((double)y / HEIGHT) * (set->y_max - set->y_min);
+	while (iter <= set->iteration)
 	{
-		temp_reel = c->z_re;
-		c->z_re = (c->z_re * c->z_re) - (c->z_im * c->z_im) + c->c_re;
-		c->z_im = (2 * temp_reel * c->z_im) + c->c_im;
-		if ((c->z_re * c->z_re + c->z_im * c->z_im) > 4.0)
+		temp_reel = set->z_re;
+		set->z_re = (
+				set->z_re * set->z_re) - (set->z_im * set->z_im) + set->c_re;
+		set->z_im = (2 * temp_reel * set->z_im) + set->c_im;
+		if ((set->z_re * set->z_re + set->z_im * set->z_im) > 4.0)
 			return (iter);
 		iter++;
 	}
 	return (iter);
 }
 
-static void	calc_pixel(t_mlx_data *data, t_set_data *c,
+static void	calc_pixel(t_mlx_data *data, t_set_data *set,
 		int (*equation)(t_set_data*, int, int))
 {
 	int		x;
@@ -69,8 +71,8 @@ static void	calc_pixel(t_mlx_data *data, t_set_data *c,
 		x = 0;
 		while (x < WIDTH)
 		{
-			iter = equation(c, x, y);
-			my_mlx_pixel_put(data, x, y, (iter * 0xEFCAE) / c->iteration);
+			iter = equation(set, x, y);
+			my_mlx_pixel_put(data, x, y, (iter * 0xEFCAE) / set->iteration);
 			x++;
 		}
 		y++;
@@ -78,10 +80,10 @@ static void	calc_pixel(t_mlx_data *data, t_set_data *c,
 	mlx_put_image_to_window(data->init, data->win, data->img, 0, 0);
 }
 
-int	calc_fractal(t_mlx_data *data, t_set_data *c)
+int	calc_fractal(t_mlx_data *data, t_set_data *set)
 {
-	if (c->set == MANDELBROT)
-		calc_pixel(data, c, mandel_equation);
-	else if (c->set == JULIA)
-		calc_pixel(data, c, julia_equation);
+	if (set->set == MANDELBROT)
+		calc_pixel(data, set, mandel_equation);
+	else if (set->set == JULIA)
+		calc_pixel(data, set, julia_equation);
 }
